a64000914b6aab89891dea21805002ceea2ff912
Conexão Code <caseiro.henrique@gmail.com>
2025-05-14 17:58:22 -0300

    Update EventoCriar.js

--- src/routes/Eventos/EventoCriar.js
+++ src/routes/Eventos/EventoCriar.js
@@ -5,6 +5,7 @@ const multer  = require('multer');
 const { connect } = require('../../config/database');
 const path    = require('path');
 const fs      = require('fs');
+const { connect } = require('../../config/database');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
